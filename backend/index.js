26e6363cb45ff85fb3b8f649191da5b1af844a17
shafna96 <sfnmail2@gmail.com>
2023-09-02 15:44:23 +0530

    backend init

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "Could not create user." });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Could not fetch users." });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
