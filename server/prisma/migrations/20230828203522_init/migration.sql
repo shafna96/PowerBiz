01ca01bc2a67c2293d7890729a5c72d227bfcee9
shafna96 <sfnmail2@gmail.com>
2023-09-12 11:05:55 +0530

    Remove 'migrations' folder from version control

--- server/prisma/migrations/20230828203522_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `password` on the `customer` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `customer` DROP COLUMN `password`;
